dec956b3e77f4fcfdbc32de44cbdcc8aa53623ab
Francesco Porto <francesco.porto97@gmail.com>
2021-03-02 12:11:02 +0100

    Start working on index

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
-use clap::{App, Arg};
-use gfa::{parser::GFAParser, gfa::GFA};
 use std::path::PathBuf;
-
+use gfa::{parser::GFAParser, gfa::GFA};
+use handlegraph::handlegraph::HandleGraph;
 use handlegraph::hashgraph::HashGraph;
+use clap::{App, Arg};
+use bv::BitVec;
+use bstr::ByteSlice;
+
+pub struct node_ref {
+    seq_idx : u64, // index among sequences
+    edge_idx : u64, // index among edges
+    count_prev : u64,
+}
 
 fn main() {
 
@@ -19,16 +27,71 @@ fn main() {
                 .required(true)
                 .takes_value(true),
         )
+        .arg(
+            Arg::with_name("kmer-length")
+                .short("k")
+                .long("kmer-length")
+                .value_name("INTEGER")
+                .help("Sets the size of each k-mer")
+                .required(true)
+                .takes_value(true),
+        )
         .get_matches();
 
     let in_path_file = matches
         .value_of("input")
-        .expect("Could not parse argument --input");
+        .unwrap();
 
+    let kmer_length = matches
+        .value_of("kmer-length")
+        .unwrap()
+        .parse::<u64>()
+        .unwrap();
+
+    // Create HashGraph from GFA
     let parser = GFAParser::new();
     let gfa : GFA<usize, ()> = parser.parse_file(&PathBuf::from(in_path_file)).unwrap();
     let graph = HashGraph::from_gfa(&gfa);
 
-    println!("{:#?}",graph);
+    let number_nodes = graph.graph.len();
+
+    // Find total length of the sequences in the graph
+    let mut total_length = 0;
+    for value in graph.handles_iter() {
+        let node = graph.get_node(&value.id()).unwrap();
+        total_length += node.sequence.len() as u64;
+    }
+
+    let mut seq_bw : BitVec = BitVec::new_fill(false,total_length+1);
+
+    let mut seq_idx : u64 = 0;
+
+    graph.handles_iter().map(|h| {
+        seq_bw[seq_idx] = true;
+
+        let node = graph.get_node(&h.id()).unwrap();
+        let seq = node.sequence.clone().to_string();
+
+        let reference = node_ref {
+            seq_idx : seq_idx,
+            edge_idx : n_edges,
+            count_prev : 0
+        };
+
+        graph.edges_iter().map(|e| {
+
+        });
+
+        n_edges += reference.count_prev;
+
+        graph.edges_iter().map(|e| {
+
+        });
+
+        seq_idx += seq.len();
+    });
+
+
+    println!("{:#?}",bitvector);
 
 }
